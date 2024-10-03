7fbe648acc20cc0b038494e1f6646a41556b9b53
Tanel Pauls <Tanel.Pauls@voco.ee>
2024-10-03 14:43:42 +0300

    Created project basic files

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express")
+const app=express()
+
+// use ejs files to prepare templates for view
+const path=require("path")
+app.set("view engine","ejs")
+app.set("views",path.join(__dirname, "views"))
+
+app.get("/", (req,res)=>{
+    res.render("index")
+})
+
+app.listen(3001,()=>{
+    console.log("Example app is started at http//localhost:3001")
+})
