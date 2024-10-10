934ba1ab3a752abe5744ad9f5eb4176b4f0025bb
Tanel Pauls <Tanel.Pauls@voco.ee>
2024-10-10 14:18:50 +0300

    Write data to file

--- index.js
+++ index.js
@@ -2,25 +2,47 @@ const express = require("express")
 const app=express()
 const fs=require('fs');
 
+const readFile=(filename)=>{
+	return new Promise((resolve, reject)=>{
+	fs.readFile(filename, "utf8", (err,data)=>{
+		if(err){
+			console.error(err);
+			return;
+		}
+		const tasks=data.split("\n")
+		resolve(tasks)
+	});
+})
+}
+
 // use ejs files to prepare templates for view
 const path=require("path")
 app.set("view engine","ejs")
 app.set("views",path.join(__dirname, "views"))
 
 app.get("/", (req,res)=>{
-
-    fs.readFile('Tasks','utf8',(err,data)=>{
-        if(err){
-            console.error(err);
-            return;
-        }
-        //console.log(data);
-        //console.log(typeof data);
-        //console.log(data.split("\n"))
-        const tasks =data.split("\n")
+    readFile('Tasks')
+        .then(tasks=>{
+        //console.log(tasks)
         res.render("index",{tasks: tasks})
-    });
-    
+    })
+})  
+
+app.use(express.urlencoded({extended: true}))
+
+app.post('/',(req,res)=>{
+    readFile('Tasks')
+        .then(tasks=>{
+        tasks.push(req.body.task)
+        const data=tasks.join("\n")
+        fs.writeFile('Tasks',data,err=>{
+            if(err){
+                console.error(err);
+                return;
+            }
+            res.redirect('/')
+        })
+    })
 })
 
 app.listen(3001,()=>{
