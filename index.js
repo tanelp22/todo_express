3064dd705ca6cf900ab8b371b20a361ada0808a3
Tanel Pauls <Tanel.Pauls@voco.ee>
2024-10-18 09:33:54 +0300

    Add error handling

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+// cd todo_express
+// npm start
+
 const express = require("express")
 const app=express()
 const fs=require('fs');
@@ -33,41 +36,57 @@ app.set("view engine","ejs")
 app.set("views",path.join(__dirname, "views"))
 
 app.get("/", (req,res)=>{
-    readFile('tasks.json')
+    readFile('./tasks.json')
         .then(tasks=>{
-        console.log(tasks)
-        res.render("index",{tasks: tasks})
+        
+        res.render("index",{
+            tasks: tasks,
+            error: null
+        })
     })
 })  
 
 app.use(express.urlencoded({extended: true}))
 
 app.post('/',(req,res)=>{
-    readFile('tasks.json')
+    let error = null
+    if(req.body.task.trim().length==0){
+        error='Please insert correct task data'
+        readFile('./tasks.json')
+        .then(tasks=>{
+            res.render('index',{
+                tasks: tasks,
+                error: error
+            })
+        })
+    }
+    else{
+        readFile('./tasks.json')
         .then(tasks=>{
         
-        let index
-        if(tasks.length===0){
-            index=0
-        }
-        else{
-            index=tasks[tasks.length-1].id+1;
-        }
-        const newTask={
-            "id":index,
-            "task":req.body.task
-        }
-        tasks.push(newTask)
-        data=JSON.stringify(tasks, null, 2)
-        writeFile('tasks.json',data)
+            let index
+            if(tasks.length===0){
+                index=1
+            }
+            else{
+                index=tasks[tasks.length-1].id+1;
+            }
+            const newTask={
+                "id":index,
+                "task":req.body.task
+            }
+            tasks.push(newTask)
+            data=JSON.stringify(tasks, null, 2)
+            writeFile('./tasks.json',data)
         
             res.redirect('/')
-    })
+        })
+    }
 })
 
 app.get('/delete-task/:taskId',(req,res)=>{
     let deletedTaskId=parseInt(req.params.taskId)
-    readFile('tasks.json')
+    readFile('./tasks.json')
     .then(tasks=>{
         tasks.forEach((task,index)=>{
             if(task.id===deletedTaskId){
@@ -75,7 +94,7 @@ app.get('/delete-task/:taskId',(req,res)=>{
             }
         })
         data=JSON.stringify(tasks,null,2)
-        writeFile('tasks.json',data)
+        writeFile('./tasks.json',data)
         res.redirect('/')
     })
 })
